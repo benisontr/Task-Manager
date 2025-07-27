8c9189f632cdd64fcf72729d8c947b9e09516a75
Benison T R <154443393+benisontr@users.noreply.github.com>
2025-07-27 14:37:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://taskmanagerullstack.netlify.app', 
+}));
+
 app.use(express.json());
 
 app.use('/api/tasks', taskRoutes);
@@ -18,9 +21,9 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 })
 .then(() => {
-  console.log(' Connected to MongoDB Atlas');
+  console.log('Connected to MongoDB Atlas');
   app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
 })
 .catch((err) => {
-  console.error(' MongoDB connection error:', err);
+  console.error('MongoDB connection error:', err);
 });
